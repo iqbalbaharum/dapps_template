ee5e45014f9d91c0d39463932471d6409a1380e2
iqbal <balainkk@gmail.com>
2021-05-11 23:08:26 +0800

    update abis, and footer

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="q-pt-md bg-secondary">
+  <div class="q-pt-md text-weight-bold">
     <div class="bn-block items-center justify-center">
-      <div class="row q-my-md justify-center">
-        <div class="col">
-          <q-img src="~assets/main/logo-transparent.png" style="max-width:30%;height:auto;" />
-        </div>
-        <div class="col row">
+      <div class="row q-my-md justify-between">
+        <img src="~assets/main/logo-transparent.png" />
+        <div>
           <div class="col">
             <ul class="bn-links">
-              <li><a class="text-white" href="#">Contact Us</a></li>
-              <li><a class="text-white" href="#">FAQ</a></li>
-              <li><a class="text-white" href="#">Twitter</a></li>
-              <li><a class="text-white" href="#">Telegram</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Telegram</a></li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="row justify-between full-width q-py-sm text-center text-white">
-        <span>© 2021 RD Labs. All rights reserved.</span>
-        <span class="q-gutter-x-md">
-          <a class="text-white" target="_blank" href="https://www.rdauditors.com/terms-and-conditions/">Terms and condition</a>
-          <a class="text-white" target="_blank" href="https://www.rdauditors.com/privacy-policy-for-rd-auditors/">Privacy policy</a>
-        </span>
-      </div>
     </div>
+    <div class="full-width q-py-sm text-white bn-footer">
+        <div class="bn-block">
+          <div class="row justify-between">
+            <span>© 2021 RD Labs. All rights reserved.</span>
+            <span class="q-gutter-x-md">
+              <a class="text-white" target="_blank" href="https://www.rdauditors.com/terms-and-conditions/">Terms and condition</a>
+              <a class="text-white" target="_blank" href="https://www.rdauditors.com/privacy-policy-for-rd-auditors/">Privacy policy</a>
+            </span>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
